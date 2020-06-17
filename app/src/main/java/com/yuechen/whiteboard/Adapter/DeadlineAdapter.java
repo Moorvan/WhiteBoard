46558eb58b389d81246f2d956ee44279ad0b4192
HavenTong <852092786@qq.com>
2020-06-17 22:07:47 +0800

    完成todo_item样式

---
+++ app/src/main/java/com/yuechen/whiteboard/Adapter/DeadlineAdapter.java
@@ -0,0 +1,72 @@
+package com.yuechen.whiteboard.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.chengang.library.TickView;
+import com.yuechen.whiteboard.Model.Deadline;
+import com.yuechen.whiteboard.R;
+
+import java.util.List;
+
+public class DeadlineAdapter extends RecyclerView.Adapter<DeadlineAdapter.ViewHolder> {
+
+    private List<Deadline> deadlines;
+
+    private Context context;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        View deadlineView;
+        TextView deadlineDateView;
+        TextView deadlineTimeView;
+        TextView deadlineContentView;
+        TextView deadlineNoteView;
+        TickView deadlineCheckView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            deadlineView = itemView;
+            deadlineDateView = deadlineView.findViewById(R.id.deadline_date);
+            deadlineTimeView = deadlineView.findViewById(R.id.deadline_time);
+            deadlineContentView = deadlineView.findViewById(R.id.deadline_content);
+            deadlineNoteView = deadlineView.findViewById(R.id.deadline_note);
+            deadlineCheckView = deadlineView.findViewById(R.id.deadline_check);
+        }
+    }
+
+    public DeadlineAdapter(List<Deadline> deadlines, Context context) {
+        this.deadlines = deadlines;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_item_layout, parent, false);
+        final ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Deadline deadline = deadlines.get(position);
+        String date = deadline.endDateTime.substring(0, 10);
+        String time = deadline.endDateTime.substring(11, 16);
+        holder.deadlineDateView.setText(date);
+        holder.deadlineTimeView.setText(time);
+        holder.deadlineContentView.setText(deadline.title);
+        holder.deadlineNoteView.setText(deadline.note);
+        holder.deadlineCheckView.setChecked(deadline.finished);
+    }
+
+    @Override
+    public int getItemCount() {
+        return deadlines.size();
+    }
+}
--- app/src/main/java/com/yuechen/whiteboard/TodoListActivity.java
+++ app/src/main/java/com/yuechen/whiteboard/TodoListActivity.java
@@ -2,10 +2,25 @@ package com.yuechen.whiteboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
+import com.yuechen.whiteboard.Adapter.DeadlineAdapter;
+import com.yuechen.whiteboard.Model.Deadline;
+import com.yuechen.whiteboard.Model.TodoItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TodoListActivity extends AppCompatActivity {
+
+    private List<Deadline> deadlines;
+    private List<TodoItem> todoItems;
+    private boolean isLesson;
+    private long folderId;
+    private String courseId;
 
     private Toolbar toolbar;
 
@@ -13,12 +28,42 @@ public class TodoListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
-        initView();
+        initToolBar();
+        Bundle bundle = getIntent().getExtras();
+        isLesson = bundle.getBoolean("isLesson");
+
+        initTodoList();
+
+        RecyclerView todoListView = findViewById(R.id.todolist);
+        StaggeredGridLayoutManager layoutManager = new
+                StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+        todoListView.setLayoutManager(layoutManager);
+        DeadlineAdapter adapter = new DeadlineAdapter(deadlines, this);
+        todoListView.setAdapter(adapter);
     }
 
-    private void initView() {
+    private void initToolBar() {
         toolbar = findViewById(R.id.todolist_toolbar);
         toolbar.setTitle("待办事项");
         toolbar.setNavigationOnClickListener((v) -> finish());
+    }
+
+    private void initTodoList() {
+//        Deadline deadline = new Deadline();
+//        deadline.endDateTime = "2020-06-15 23:59:59";
+//        deadline.title = "编译原理";
+//        deadline.note = "尽快保质保量完成";
+//        deadline.finished = false;
+//        Deadline deadline1 = new Deadline();
+//        deadline1.endDateTime = "2020-06-16 23:59:59";
+//        deadline1.title = "Android";
+//        deadline1.note = "快交";
+//        deadline1.finished = true;
+//        deadlines = new ArrayList<>();
+//        deadlines.add(deadline);
+//        deadlines.add(deadline1);
+        if (isLesson) {
+
+        }
     }
 }
